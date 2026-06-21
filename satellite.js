368fd322bb75a6e6e983469d78a2e6734a5e1094
eliasmvargas <elias.mariano.vargas@gmail.com>
2026-06-21 09:12:14 -0600

    Fixed satellite aspect ratio

--- satellite.js
+++ satellite.js
@@ -1,27 +1,31 @@
 
 export class Satellite{
     constructor(max_major, max_minor, orbitSpeed){
-        // spacial parameters
-        this.max_major = max_major; // maximum major-axis value - x
-        this.max_minor = max_minor; // maximum minor-axis value - y
-        this.angle = 0;
-        this.orbitSpeed = orbitSpeed;
-        this.x_position = this.max_major;
-        this.y_position = 0;
+            // spacial parameters
+            this.max_major = max_major; 
+            this.max_minor = max_minor; 
+            this.angle = 0;
+            this.orbitSpeed = orbitSpeed;
+            this.x_position = this.max_major;
+            this.y_position = 0;
 
-        // image asset loading
+            // image asset loading
+            this.image = new Image();
+            this.image.src = 'resources/SVG/satellite.svg'
 
-        this.image = new Image();
-        this.image.src = 'resources/SVG/satellite.svg'
+            // Your actual SVG dimensions
+            this.originalWidth = 760.88;
+            this.originalHeight = 316.44;
 
-        this.imageWidth = 80;
-        this.imageHeight = 80;
+            // Calculate the drawing dimensions to max-width of 150px while keeping the ratio
+            this.drawWidth = 100; 
+            this.drawHeight = this.originalHeight * (this.drawWidth / this.originalWidth); 
 
-        this.isLoaded = false;
-        this.image.onload = () => {
-            this.isLoaded = true;
+            this.isLoaded = false;
+            this.image.onload = () => {
+                this.isLoaded = true;
+            }
         }
-    }
 
     update(deltaTime, centerX, centerY){
         this.angle += this.orbitSpeed * deltaTime;
@@ -33,29 +37,32 @@ export class Satellite{
     }
 
     draw(context){
-        if (this.isLoaded) {
+            if (this.isLoaded) {
+                context.save();
 
-            context.save();
+                // Move the canvas origin to the satellite's position
+                context.translate(this.x_position, this.y_position);
+                
+                // Rotate to match the orbit angle
+                context.rotate(this.angle + Math.PI - (Math.PI / 2));
+        
+                // Draw the image centered on the new origin using the scaled math
+                context.drawImage(
+                    this.image,
+                    -(this.drawWidth / 2),  // Center horizontally
+                    -(this.drawHeight / 2), // Center vertically
+                    this.drawWidth,         // 150
+                    this.drawHeight         // ~62.38
+                );
 
-            context.translate(this.x_position, this.y_position);
-            context.rotate(this.angle + Math.PI - (Math.PI / 2));
-    
-            context.drawImage(
-                this.image,
-                -(this.imageWidth / 2),  
-                -(this.imageHeight / 2), 
-                this.imageWidth,
-                this.imageHeight
-            );
-
-            context.restore();
-        } else {
-            // Fallback dot
-            context.beginPath();
-            context.arc(this.x_position, this.y_position, 5, 0, Math.PI * 2);
-            context.fillStyle = "#FFFFFF";
-            context.fill();
+                context.restore();
+            } else {
+                // Fallback dot
+                context.beginPath();
+                context.arc(this.x_position, this.y_position, 5, 0, Math.PI * 2);
+                context.fillStyle = "#FFFFFF";
+                context.fill();
+            }
         }
-    }
 
 }
